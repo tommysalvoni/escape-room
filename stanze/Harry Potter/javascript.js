71038dbd1f686f6677c872d0cfae6a7b0f2da1be
tommysalvoni <tommysalvoni@gmail.com>
2022-05-17 13:31:22 +0200

    primo commit

---
+++ stanze/Harry Potter/javascript.js
@@ -0,0 +1,18 @@
+function openNav() {
+    let x=document.getElementById("mySidebar").style.width
+    if(x!="250px"){
+      document.getElementById("mySidebar").style.width = "250px";    
+      document.getElementById("main").style.marginLeft = "250px";
+      document.getElementById("titolo").style.marginRight = "250px";
+    }else{
+   closeNav();
+    }
+    
+    }
+    
+  
+    function closeNav() {
+      document.getElementById("mySidebar").style.width = "0";
+      document.getElementById("main").style.marginLeft = "0";
+      document.getElementById("titolo").style.marginRight = "0px";
+    }
